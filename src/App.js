bc41186a99e1a7259b8319e0411c1dcef526ab92
thesmartmover <alltimehodl@gmail.com>
2026-03-31 22:17:27 +0800

    001

--- src/App.js
+++ src/App.js
@@ -5,11 +5,37 @@ import ProductCard from './ProductCard';
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <ProductCard product={this.props.product} />
-            </div>
+            <>
+                <header style={styles.header}>tnShop</header>
+                <main style={styles.main}>
+                    <ProductCard product={this.props.product} />
+                </main>
+                <footer style={styles.footer}>
+                    &copy; {new Date().getFullYear()}, @thesmartmoover
+                </footer>
+            </>
         );
     }
 }
 
 export default App;
+
+const styles = {
+    header: {
+        backgroundColor: '#222222',
+        color: '#fff',
+        minHeight: '50px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '1.2rem',
+        fontWeight: 'bold'
+    },
+    main: {
+        padding: '10px 10%'
+    },
+    footer: {
+        padding: '0 10%',
+        marginTop: '50px'
+    }
+};
